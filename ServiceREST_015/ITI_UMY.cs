2c5ed96e7a7644440bb4bb98b60c831c3084abe0
Muhammad Dzalfiqri Sa'bani <muhdzalfikri@gmail.com>
2020-12-25 15:16:47 +0700

    Revert "Add Service Delete"
    This reverts commit 79d23a4e9bf227d509c73067669aaace015e1f04.

--- ServiceREST_015/ITI_UMY.cs
+++ ServiceREST_015/ITI_UMY.cs
@@ -28,9 +28,6 @@ namespace ServiceREST_015
         [WebInvoke(Method = "POST", UriTemplate = "UpdateMahasiswa", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
         string UpdateMahasiswa(Mahasiswa mhs);
 
-        [OperationContract]
-        [WebInvoke(Method = "POST", UriTemplate = "DeleteMahasiswa", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
-        string DeleteMahasiswa(string nim);
     }
 
     [DataContract]
